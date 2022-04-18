3666d444fae88f097f66b4850aa8e5231dcbe113
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-04-18 19:24:56 -0300

    feat: adcionado _document page

--- front-on-food/pages/descobrir/[city].tsx
+++ front-on-food/pages/descobrir/[city].tsx
@@ -1,23 +1,33 @@
-import { useRouter } from "next/router";
+import Head from "next/head";
 import { PageTemplate, DishList } from "@/components";
 import Styles from "../../styles/discovery.module.css"
 import { api } from "@/services";
 import { CityPros, ParamsStaticProps, PageDiscoverProps } from "@/types";
 
 
-export default function Descobrir(props: PageDiscoverProps){
+export default function Descobrir(props: PageDiscoverProps) {
     const { city } = props;
-    
+
     return (
-        <PageTemplate>
-            <div className={Styles.content}>
-                <h1>Opções na região de {city.name}</h1>
-                <p>Encontramos {city.catalogEstimated} opções</p>
-                <div className={Styles.items}>
-                    <DishList citySlug={city.slug}/>
+        <>
+
+            <Head>
+                <title>Opções em {city.name} - OnFood App</title>
+                <meta name="description" content={`Encontre opções em Delivery próximos à você em ${city.name}`} />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <PageTemplate>
+                <div className={Styles.content}>
+                    <h1>Opções na região de {city.name}</h1>
+                    <p>Encontramos {city.catalogEstimated} opções</p>
+                    <div className={Styles.items}>
+                        <DishList citySlug={city.slug} />
+                    </div>
                 </div>
-            </div>
-        </PageTemplate>
+            </PageTemplate>
+
+        </>
     )
 }
 
@@ -27,7 +37,7 @@ export async function getStaticPaths() {
     const cities = response.data;
 
     const urls = cities.map((city: CityPros) => ({
-        params : {
+        params: {
             city: city.slug,
         },
     }));
@@ -36,9 +46,9 @@ export async function getStaticPaths() {
         paths: urls,
         fallback: false,
     };
-} 
+}
 
-export async function getStaticProps({params}: ParamsStaticProps) {
+export async function getStaticProps({ params }: ParamsStaticProps) {
     const citySlug = params?.city as string;
     const response = await api.get(`/cities?citySlug=${citySlug}`);
 
